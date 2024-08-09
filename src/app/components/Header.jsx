bdcaa30eec50ed99cc498f28ab7f4ac29e7b6a57
Madhusudan Kulkarni <96856144+madhusudan-kulkarni@users.noreply.github.com>
2024-08-09 16:01:40 +0530

    setup user authentication using WorkOS

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,17 +1,45 @@
+import { getSignInUrl, getUser, signOut } from "@workos-inc/authkit-nextjs";
 import Link from "next/link";
-export default function Header() {
+
+export default async function Header() {
+  const { user } = await getUser();
+  const signInUrl = await getSignInUrl();
+
   return (
     <header>
-      <div className="flex items-center justify-between mx-auto my-4 container">
-        <Link href="/" className="font-bold text-gray-900 text-xl">
+      <div className="container flex items-center justify-between mx-auto my-4">
+        <Link href={"/"} className="font-bold text-xl">
           Job Board
         </Link>
-        <nav className="flex gap-3 *:py-2 *:px-4 *:rounded-md">
-          <Link href="/login" className="bg-gray-200">
-            Login
-          </Link>
-          <Link href="/post-a-job" className="bg-blue-600 text-white">
-            Post a Job
+        <nav className="flex gap-2">
+          {!user && (
+            <Link
+              className="rounded-md bg-gray-200 py-1 px-2 sm:py-2 sm:px-4"
+              href={signInUrl}
+            >
+              Login
+            </Link>
+          )}
+          {user && (
+            <form
+              action={async () => {
+                "use server";
+                await signOut();
+              }}
+            >
+              <button
+                type="submit"
+                className="rounded-md bg-gray-200 py-1 px-2 sm:py-2 sm:px-4"
+              >
+                Logout, {user.firstName}
+              </button>
+            </form>
+          )}
+          <Link
+            className="rounded-md py-1 px-2 sm:py-2 sm:px-4 bg-blue-600 text-white"
+            href={"/new-listing"}
+          >
+            Post a job
           </Link>
         </nav>
       </div>
